0099f89cec4d20fbfe05c5034cac5df139470a35
openMINDS pipeline <openminds@ebrains.eu>
2025-11-19 07:36:56 +0000

    build triggered by push

--- openminds/__init__.py
+++ openminds/__init__.py
@@ -4,7 +4,7 @@ openMINDS Python package
 # Copyright (c) 2023 openMetadataInitiative
 """
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 from .base import Node, EmbeddedMetadata, LinkedMetadata, IRI, Link
 from .collection import Collection
