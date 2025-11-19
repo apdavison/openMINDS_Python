814cb5dc61dd23d9bec76816c679e89559400339
Andrew Davison <andrew.davison@cnrs.fr>
2025-11-19 10:01:55 +0100

    post-release increment of version identifier

--- build.py
+++ build.py
@@ -120,7 +120,7 @@ for version, module_list in openminds_modules.items():
 
 env = Environment(loader=FileSystemLoader(os.path.dirname(os.path.realpath(__file__))), autoescape=select_autoescape())
 context = {
-    "version": "0.4.0",
+    "version": "0.4.1.dev.0",
 }
 if args.branch == "development":
     context["version"] += ".dev"
